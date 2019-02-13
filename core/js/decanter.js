306e61939930565f14b681dddb542c7124c26e99
JB Christy <jb.christy@stanford.edu>
2019-02-12 16:34:30 -0800

    re-refactor JS into classes

--- core/js/decanter.js
+++ core/js/decanter.js
@@ -7,119 +7,7 @@ document.addEventListener( "DOMContentLoaded", event => {
   NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
 
   //////
-  // Functions
-
-  const showingDesktopNav = () => getComputedStyle( aMobileToggle ).display === 'none';
-
-  const isExpanded = elem => elem.getAttribute('aria-expanded') === 'true';
-
-  const setAriaExpanded = ( elem, value ) => { elem.setAttribute('aria-expanded', value); };
-
-  const hasSubNav = elem => ( elem.tagName.toUpperCase() == 'LI' && elem.lastElementChild.tagName.toUpperCase() === 'UL' );
-
-  const getNextLevelItems = elem => elem.querySelectorAll( elem.tagName + " > ul > li" );
-
-  const getNav = elem => {
-    console.debug( 'getNav: elem: ', elem );
-    do {
-      elem = elem.parentElement;
-      // console.debug( 'getNav: elem: ', elem );
-    } while ( elem && !elem.classList.contains( navClass ) );
-    return elem || false;
-  };
-
-  const getSubNav = elem => {
-    console.debug( 'getSubNav: elem: ', elem );
-    do {
-      elem = elem.parentElement;
-      // console.debug( 'getSubNav: elem: ', elem );
-    } while ( elem && !hasSubNav( elem ) );
-    return elem || false;
-  };
-
-  const openSubNav = ( trigger, level = 'lv1' ) => { //// TODO: remove level when scss no longer refers to it
-    closeAllSubNavs();
-
-    if ( trigger ) {
-      trigger.classList.add( 'su-main-nav__item-' + level + '--expanded' );
-      setAriaExpanded( trigger, 'true' );
-
-      // set focus on the first link in the newly opened subnav
-      let firstLink = trigger.querySelector( 'ul > li' ).firstElementChild; // no idea why 'ul > li > a' doesn't work???
-      firstLink.focus();
-    }
-  };
-
-  const closeSubNav = ( trigger, level = 'lv1' ) => {
-    if ( trigger ) {
-      trigger.classList.remove( 'su-main-nav__item-' + level + '--expanded' );
-      setAriaExpanded( trigger, 'false' );
-    }
-  };
-
-  // click handler for subnav triggers
-  const toggleSubNav = event => {
-    event = event || window.event;
-    event.preventDefault();
-    event.stopPropagation();
-
-    const target = event.target || event.srcElement; // the <a> element that was clicked
-    const item = target.parentElement; // the <li> that contains the <a> element that was clicked
-
-    if ( isExpanded( item ) ) {
-      closeSubNav( item );
-    }
-    else {
-      openSubNav( item );
-    }
-  };
-
-  const openMobileNav = ( nav, toggle ) => {
-    closeAllMobileNavs();
-
-    if ( nav ) {
-      toggle = toggle || nav.querySelector( nav.tagName + ' > button' );
-      toggle.innerHTML = 'Close';
-      setAriaExpanded( nav, 'true' );
-      setAriaExpanded( toggle, 'true' );
-      nav.querySelector( 'li > a' ).focus(); // Focus on the first top level link
-    }
-  };
-
-  const closeMobileNav = ( nav, toggle ) => {
-    if ( nav ) {
-      toggle = toggle || nav.querySelector( nav.tagName + ' > button' );
-      toggle.innerHTML = 'Menu';
-      setAriaExpanded( nav, 'false' );
-      setAriaExpanded( toggle, 'false' );
-    }
-  };
-
-  // click handler for hamburger icon (mobile menu toggle)
-  const toggleMobileNav = event => {
-    event = event || window.event;
-    event.preventDefault();
-    event.stopPropagation();
-
-    const toggle = event.target || event.srcElement; // the <button> element that was clicked
-    const nav = toggle.parentElement;
-
-    if ( isExpanded( nav ) ) {
-      closeMobileNav( nav, toggle );
-    }
-    else {
-      openMobileNav( nav, toggle );
-    }
-  };
-
-  const closeAllSubNavs = () => { subNavs.forEach( subNav => { closeSubNav( subNav ); } ); };
-
-  const closeAllMobileNavs = () => { navs.forEach( ( nav ) => { closeMobileNav( nav, nav.querySelector( 'button' ) ); } ); };
-
-  //////
-  // Keyboard interaction
-
-  // helper functions
+  // Keyboard helper functions
   const isHome = theKey => ( theKey === 'Home' || theKey === 122 );
   const isEnd = theKey => ( theKey === 'End' || theKey === 123 );
   const isTab = theKey => ( theKey === 'Tab' || theKey === 9 );
@@ -131,78 +19,358 @@ document.addEventListener( "DOMContentLoaded", event => {
   const isUpArrow = theKey => ( theKey === 'ArrowUp' || theKey === 'Up' || theKey === 38 );
   const isDownArrow = theKey => ( theKey === 'ArrowDown' || theKey === 'Down' || theKey === 40 );
 
-  // keydown handlers
-  const keyDownTopNav = event => {
-    event = event || window.event;
-    event.stopPropagation();
 
-    const target = event.target || event.srcElement; // the element that had focus when the key was pressed (presumably an <a> element)
-    const theKey = event.key || event.keyCode;
-    const theNav = getNav( target );
-    const theSubNav = getSubNav( target );
-    const topLevelItems = getNextLevelItems( theNav );
-    const firstLink = topLevelItems[0].querySelector( 'a' );
-    const lastLink = topLevelItems[ topLevelItems.length - 1 ].querySelector( 'a' );
-    const desktop = showingDesktopNav();
+  //////
+  // Helper classes
 
-    console.debug( 'You pressed the ', theKey, ' key on target ', target );
-    console.debug( 'theNav: '   , theNav    );
-    console.debug( 'theSubNav: ', theSubNav );
-    console.debug( 'firstLink: ', firstLink );
-    console.debug( 'firstLink: ', firstLink );
-    console.debug( 'lastLink: ' , lastLink  );
+  class NavItem {
+    constructor( item, nav ) {
+      this.item = item;
+      this.nav = nav; // parent elem that contains this item
+      this.link = this.item.querySelector( 'a' );
+      this.subNav = null;
 
+      this.item.addEventListener( 'keydown', this );
 
-    if ( isEsc( theKey ) ) {
-      if ( desktop ) closeAllSubNavs();
-      else closeAllMobileNavs();
+      if ( this.isSubNavTrigger() ) {
+        this.subNav = new Nav( this, this.nav.depth + 1 );
+        theSubNavs.push( this ); // maintain globsl list of subnavs for closeAllSubNavs()
+        this.item.addEventListener( 'click', this );
+      }
     }
-    else if ( isHome( theKey ) ) {
-      firstLink.focus(); // Focus on the first top level menu link when HOME button is pressed
+
+    isSubNavTrigger() {
+      return this.item.lastElementChild.tagName.toUpperCase() === 'UL';
+    };
+
+    isSubNavItem() {
+      return this.isSubNavTrigger() || ( this.nav.depth > 0 );
     }
-    else if ( isEnd( theKey ) ) {
-      lastLink.focus(); // Focus on the last top level menu link when END button is pressed
+
+    isExpanded() {
+      return this.item.getAttribute('aria-expanded') === 'true';
     }
-    else if ( ( isSpace( theKey ) || isEnter( theKey ) ) && theSubNav ) {
+
+    setExpanded( value ) {
+      this.item.setAttribute( 'aria-expanded', value );
+    }
+
+    isFirstItem() {
+      return this.nav.items.indexOf( this ) == 0;
+    }
+
+    isLastItem() {
+      return this.nav.items.indexOf( this ) == this.nav.items.length - 1;
+    }
+
+    openSubNav() {
+      closeAllSubNavs();
+
+      if ( this.isSubNavTrigger() ) {
+        this.item.classList.add( 'su-main-nav__item-lv1--expanded' ); //// TODO: remove lv1 when scss no longer refers to it
+        this.setExpanded( 'true' );
+        this.subNav.focusOn( 'first' );
+      }
+    };
+
+    closeSubNav( focusOnTrigger = false ) {
+      if ( this.isSubNavTrigger() ) {
+        this.item.classList.remove( 'su-main-nav__item-lv1--expanded' ); //// TODO: remove lv1 when scss no longer refers to it
+        this.setExpanded( 'false' );
+        if ( focusOnTrigger ) this.link.focus();
+      }
+      else if ( this.isSubNavItem() ) {
+        this.nav.elem.closeSubNav( focusOnTrigger ); // this.nav.elem should be a subNavTrigger
+      }
+    };
+
+    //////
+    // Event handlers
+
+    // handler for all events attached to an instance of this class
+    // this method must exist when events are bound to an instance of a class (vs a function)
+    handleEvent( event ) {
+      event = event || window.event;
+      // if this class has an onevent method, e.g. onClick, onKeydown, invoke it
+      const handler = 'on' + event.type.charAt(0).toUpperCase() + event.type.slice(1);
+      if ( typeof  this[ handler ] === 'function' ) {
+        return this[ handler ]( event );
+      }
+    }
+
+    // keydown handler
+    onKeydown( event ) {
+      const theKey  = event.key || event.keyCode;
+      const shifted = event.shiftKey;
+
+      if ( !isTab( theKey ) ) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      if ( isEsc( theKey ) ) {
+        closeAllSubNavs();
+        if ( !this.nav.desktopNav() ) closeAllMobileNavs();
+      }
+      else if ( isSpace( theKey ) ) {
+        if ( this.isSubNavTrigger() ) {
+          this.openSubNav();
+        }
+      }
+      else if ( isDownArrow( theKey ) ) {
+        if ( this.nav.desktopNav() ) {
+          if ( this.isSubNavTrigger() ) {
+            this.openSubNav();
+          }
+        }
+        else {
+          this.nav.focusOn( 'next', this );
+        }
+      }
+      else if ( isUpArrow( theKey ) ) {
+        if ( this.nav.desktopNav() ) {
+          if ( this.isSubNavItem() ) {
+            this.closeSubNav( true );
+          }
+        }
+        else {
+          this.nav.focusOn( 'prev', this );
+        }
+      }
+      else if ( isLeftArrow( theKey ) ) {
+        if ( this.nav.desktopNav() ) {
+          this.nav.focusOn( 'prev', this );
+        }
+        else {
+          if ( this.isSubNavTrigger() ) {
+            this.closeSubNav();
+          }
+        }
+      }
+      else if ( isRightArrow( theKey ) ) {
+        if ( this.nav.desktopNav() ) {
+          this.nav.focusOn( 'next', this );
+        }
+        else {
+          if ( this.isSubNavTrigger() ) {
+            this.openSubNav();
+          }
+        }
+      }
+      else if ( isHome( theKey ) ) {
+        this.nav.focusOn( 'first' );
+      }
+      else if ( isEnd( theKey ) ) {
+        this.nav.focusOn( 'last' );
+      }
+      else if ( isTab( theKey ) ) {
+        if ( this.isSubNavItem() && this.isLastItem() ) {
+          this.closeSubNav();
+        }
+      }
+    }
+
+    // click handler - only bound to subnav triggers
+    onClick( event ) {
       event.preventDefault();
-      openSubNav( theSubNav );
-    }
-  };
+      event.stopPropagation();
 
+      if ( this.isExpanded() ) {
+        this.closeSubNav();
+      }
+      else {
+        this.openSubNav();
+      }
+    }
+
+  }
+
+  class Nav {
+    constructor( elem, depth ) {
+      this.elem = elem;
+      this.depth = depth;
+      if ( elem instanceof NavItem ) elem = elem.item;
+      this.toggle = elem.querySelector( elem.tagName + " > button" );
+      this.toggleText = this.toggle ? this.toggle.innerText : '';
+      this.items = [];
+      let items = elem.querySelectorAll( elem.tagName + " > ul > li" );
+      items.forEach( item => { this.items.push( new NavItem( item, this ) ); } );
+
+      if ( this.toggle ) {
+        this.toggle.addEventListener( 'click', this );
+      }
+    }
+
+    getTopLevelNav() {
+      let nav = this;
+      while ( nav.elem instanceof NavItem ) {
+        nav = nav.elem.nav;
+      }
+      // for ( let nav = this; nav.elem instanceof NavItem; nav = nav.elem.nav );
+      return nav;
+    }
+
+    desktopNav() {
+      return getComputedStyle( this.getTopLevelNav().toggle ).display === 'none';
+    }
+
+    isExpanded() {
+      return this.elem instanceof NavItem ? this.elem.isExpanded() : this.elem.getAttribute('aria-expanded') === 'true';
+    }
+
+    setExpanded( value ) {
+      this.elem instanceof NavItem ? this.elem.setExpanded( value ) : this.elem.setAttribute( 'aria-expanded', value );
+      if ( this.toggle ) this.toggle.setAttribute( 'aria-expanded', value );
+    }
+
+    firstItem() {
+      return this.items.length ? this.items[ 0 ] : null;
+    }
+
+    lastItem() {
+      return this.items.length ? this.items[ this.items.length - 1 ] : null;
+    }
+
+    firstLink() {
+      return this.items.length ? this.firstItem().link : null;
+    }
+
+    lastLink() {
+      return this.items.length ? this.lastItem().link : null;
+    }
+
+    focusOn( link, currentItem = null ) {
+      var currentIndex, lastIndex = null;
+      if ( currentItem ) {
+        currentIndex = this.items.indexOf( currentItem );
+        lastIndex = this.items.length - 1;
+      }
+      switch ( link ) {
+        case 'first':
+          this.firstLink().focus();
+          break;
+        case 'last':
+          this.lastLink().focus();
+          break;
+        case 'next':
+          if ( currentIndex == lastIndex ) {
+            this.firstLink().focus();
+          }
+          else {
+            this.items[ currentIndex + 1 ].link.focus();
+          }
+          break;
+        case 'prev':
+          if ( currentIndex == 0 ) {
+            this.lastLink().focus();
+          }
+          else {
+            this.items[ currentIndex - 1 ].link.focus();
+          }
+          break;
+        default:
+          if ( Number.isInteger( link ) && link >= 0 && link < this.items.length ) {
+            this.items[ link ].link.focus();
+          }
+          break;
+      }
+    }
+
+    openMobileNav() {
+      closeAllMobileNavs();
+
+      this.setExpanded( 'true' );
+      this.focusOn( 'first' ); // Focus on the first top level link
+      this.toggle.innerText = 'Close';
+    };
+
+    closeMobileNav() {
+      this.setExpanded( 'false' );
+      this.toggle.innerText = this.toggleText;
+    };
+
+    // Event handlers
+
+    // handler for all events attached to an instance of this class
+    // this method must exist when events are bound to an instance of a class (vs a function)
+    handleEvent( event ) {
+      event = event || window.event;
+      // if this class has an onEvent method, e.g. onClick, onKeydown, invoke it
+      const handler = 'on' + event.type.charAt(0).toUpperCase() + event.type.slice(1);
+      if ( typeof  this[ handler ] === 'function' ) {
+        const target = event.target || event.srcElement; // the element that was clicked
+        return this[ handler ]( event, target );
+      }
+    }
+
+    onClick( event ) {
+      console.debug( 'Nav:onclick event: ', event );
+      const target = event.target || event.srcElement; // the element that was clicked
+      console.debug( 'You clicked on ', target );
+
+      switch ( target.tagName.toUpperCase() ) {
+        case 'BUTTON':
+          console.debug( 'You clicked on a BUTTON.' );
+          if ( target == this.toggle ) {
+            console.debug( 'You clicked on the mobile toggle.' );
+            event.preventDefault();
+            event.stopPropagation();
+            if ( this.isExpanded() ) {
+              this.closeMobileNav();
+            }
+            else {
+              this.openMobileNav();
+            }
+          }
+          break;
+        case 'A':
+          this.items.forEach( item => {
+            if ( target == item.link && item.isSubNavTrigger() ) {
+              console.debug( 'You clicked on a subnav trigger.' );
+            }
+            else {
+              console.debug( 'You clicked on some other A element.' );
+            }
+          } );
+          break;
+        default:
+          console.debug( 'You clicked on an unhandled element: ', target.tagName.toUpperCase() );
+      }
+    }
+
+  }
+
+  //////
+  // Functions
+
+  const closeAllSubNavs = () => { theSubNavs.forEach( subNav => { subNav.closeSubNav(); } ); };
+
+  const closeAllMobileNavs = () => { theNavs.forEach( theNav => { theNav.closeMobileNav(); } ); };
 
   //////
   // Code
 
-  const navs = document.querySelectorAll( '.' + navClass ); // all main nav's
-  let aMobileToggle = null; // the first mobile toggle we find - used to determine if we're displaying mobile or desktop nav
-  let subNavs = []; // array of subnavs to be closed by closeAllSubNavs();
+  // globals
+  let theNavs = []; // global record of all main navs - used by closeAllMobileNavs
+  let theSubNavs = []; // global record of all sub navs - used by closeAllSubNavs
 
-  // if more than 1 main nav, assign lower z-index to main navs lower on the page so they don't cover each other up
-  if ( navs.length > 1 ) {
-    const firstZindex = getComputedStyle( navs[ 0 ], null ).zIndex;
-    for ( let i = 1; i < navs.length; i++ ) {
-      navs[ i ].style.zIndex = firstZindex - 300 * i;
-    }
-  }
+  const navs = document.querySelectorAll( '.' + navClass ); // all main elem's
+  // process each elem element
 
-  // process each nav element
+  var firstZindex;
   navs.forEach( ( nav, index ) => {
-    const mobileToggle = nav.querySelector( nav.tagName + " > button" );
-    const topLevelItems = getNextLevelItems( nav );
+    const theNav = new Nav( nav, 0 );
+    theNavs.push( theNav );
 
-    mobileToggle.addEventListener( 'click', toggleMobileNav );
-    if ( aMobileToggle === null ) aMobileToggle = mobileToggle;
-
-    topLevelItems.forEach( item => {
-      item.addEventListener( 'keydown', keyDownTopNav );
-
-      if ( hasSubNav( item ) ) {
-        subNavs.push( item ); // remember the subnavs for closeAllSubNavs()
-        item.addEventListener( 'click', toggleSubNav );
-      }
-    } );
+    if ( index === 0 ) {
+      firstZindex = getComputedStyle( nav, null ).zIndex;
+    }
+    else {
+      nav.style.zIndex = firstZindex - 300 * index;
+    }
   } ); // navs.forEach
 
+  // clicking anywhere outside a elem closes all navs
   document.addEventListener('click', ( event ) => {
     closeAllSubNavs();
     closeAllMobileNavs();
